ad1bef3027afe932674a9296d69261315ecefa69
thangbk2209 <thang.nd@financialdeepmind.com>
2020-07-29 11:04:30 +0700

    Initial commit

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
+CORE_DATA_DIR = PROJECT_DIR + '/{}'.format('data')
+
---
+++ manager.py
@@ -0,0 +1,6 @@
+"""
+  Author:  bkc@data_analysis
+  Project: autoencoder_ng
+  Created: 7/29/20 10:51
+  Purpose: START SCRIPT FOR AUTOENCODER_NG PROJECT
+"""
