78a2915d924c4006ef320bd8dced6545d43afcf2
hustducnv <hust.ducnv@gmail.com>
2020-08-18 16:06:19 +0700

    i have no idea

--- config.py
+++ config.py
@@ -10,8 +10,8 @@ class Config:
     DATA_FILE = os.path.join(CORE_DATA_DIR, 'input_data', 'google_trace', 'all_jobs', '5_mins.csv')
     INPUT_COLS, PREDICT_COLS = [0], [0]
     LEARNING_RATE = 3e-4
-    SPLIT_RATIO = (0.7, 0.15, 0.15)
-    INPUT_TIME_STEPS = 20
+    SPLIT_RATIO = (0.8, 0.1, 0.1)
+    INPUT_TIME_STEPS = 15
     PREDICT_TIME_STEPS = 1
     FEATURES = 1
     VERBOSE = 2
@@ -22,8 +22,8 @@ class Config:
         'layer_units_encoder': [128, 32],
         'layer_units_decoder': [128, 32],
         'time_steps_decoder': 8,
-        'drop_out': 0.2,
-        'recurrent_drop_out': 0.2,
+        'drop_out': 0.4,
+        'recurrent_drop_out': 0.4,
         'activation': 'tanh',
         'recurrent_activation': 'sigmoid',
         'batch_size': 256,
@@ -34,7 +34,7 @@ class Config:
 
     MLP_CONFIG = {
         'hidden_layer_units': [128, 64, 16],
-        'drop_out': 0.2,
+        'drop_out': 0.4,
         'hidden_activation': 'relu',
         'batch_size': 256,
         'optimizer': 'adam',
