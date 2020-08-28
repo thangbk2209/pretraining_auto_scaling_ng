04cffbe363e6445aaf26cd1f07573e327b97993d
hustducnv <hust.ducnv@gmail.com>
2020-08-28 23:16:41 +0700

    add LSTMAutoencoderV2

--- src/models/autoencoder.py
+++ src/models/autoencoder.py
@@ -5,7 +5,7 @@ from tensorflow.keras.layers import (
 )
 
 
-class LSTMAutoEncoder(object):
+class LSTMAutoEncoderV1(object):
     def __init__(self,
                  inputs_shape,  # (time_steps, features)
                  layer_units_encoder,
@@ -34,8 +34,8 @@ class LSTMAutoEncoder(object):
         decoder_num_layers = len(self.layer_units_decoder)
 
         # encoder
-        encoder_inputs = Input(shape=self.inputs_shape)
-        z = encoder_inputs
+        encoder_input = Input(shape=self.inputs_shape)
+        z = encoder_input
         for i in range(encoder_num_layers):
             z = LSTM(
                 units=self.layer_units_encoder[i],
@@ -47,12 +47,12 @@ class LSTMAutoEncoder(object):
                 return_state=(i == encoder_num_layers - 1)
             )(z)
 
-        encoder_outputs, state_h, state_c = z
+        encoder_output, state_h, state_c = z
         encoder_state = [state_h, state_c]
-        encoder = Model(inputs=[encoder_inputs], outputs=[encoder_outputs])
+        encoder = Model(inputs=[encoder_input], outputs=[encoder_output])
 
         # autoencoder
-        decoder_inputs = Input(shape=(self.timesteps_decoder, 1))
+        decoder_input = Input(shape=(self.timesteps_decoder, 1))
         v = LSTM(
             units=self.layer_units_decoder[0],
             activation=self.activation,
@@ -60,7 +60,7 @@ class LSTMAutoEncoder(object):
             dropout=self.drop_out,
             recurrent_dropout=self.recurrent_drop_out,
             return_sequences=True
-        )(decoder_inputs, initial_state=encoder_state)
+        )(decoder_input, initial_state=encoder_state)
 
         for i in range(1, decoder_num_layers):
             v = LSTM(
@@ -73,5 +73,66 @@ class LSTMAutoEncoder(object):
             )(v)
         v = TimeDistributed(Dense(1))(v)
 
-        autoencoder = Model(inputs=[encoder_inputs, decoder_inputs], outputs=[v])
+        autoencoder = Model(inputs=[encoder_input, decoder_input], outputs=[v])
+        return encoder, autoencoder
+
+
+class LSTMAutoEncoderV2(object):
+    def __init__(self,
+                 inputs_shape,  # (time_steps, features)
+                 layer_units_encoder,
+                 layer_units_decoder,
+                 timesteps_decoder,
+                 drop_out=.0,
+                 recurrent_drop_out=.0,
+                 activation='tanh',
+                 recurrent_activation='sigmoid'):
+
+        self.inputs_shape = inputs_shape
+        self.layer_units_encoder = layer_units_encoder
+        self.layer_units_decoder = layer_units_decoder
+        self.timesteps_decoder = timesteps_decoder
+        self.drop_out = drop_out
+        self.recurrent_drop_out = recurrent_drop_out
+        self.activation = activation
+        self.recurrent_activation = recurrent_activation
+        self.encoder, self.model = self._build_models()
+
+    def _build_models(self):
+        encoder_num_layers = len(self.layer_units_encoder)
+        decoder_num_layers = len(self.layer_units_decoder)
+
+        # encoder
+        encoder_input = Input(shape=self.inputs_shape)
+        z = encoder_input
+        encoder_output = None
+        encoder_states = list()
+        for i in range(encoder_num_layers):
+            z, state_h, state_c = LSTM(
+                units=self.layer_units_encoder[i],
+                activation=self.activation,
+                recurrent_activation=self.recurrent_activation,
+                dropout=self.drop_out,
+                recurrent_dropout=self.recurrent_drop_out,
+                return_sequences=(i != encoder_num_layers - 1),
+                return_state=True
+            )(z)
+            encoder_states.append([state_h, state_c])
+        encoder = Model(inputs=[encoder_input], outputs=[z])
+
+        # decoder
+        decoder_input = Input(shape=(self.timesteps_decoder, 1))
+        v = decoder_input
+        for i in range(decoder_num_layers):
+            v = LSTM(
+                units=self.layer_units_decoder[i],
+                activation=self.activation,
+                recurrent_activation=self.recurrent_activation,
+                dropout=self.drop_out,
+                recurrent_dropout=self.recurrent_drop_out,
+                return_sequences=True
+            )(v, initial_state=encoder_states[i])
+        v = TimeDistributed(Dense(1))(v)
+
+        autoencoder = Model(inputs=[encoder_input, decoder_input], outputs=[v])
         return encoder, autoencoder
--- src/models/mlp.py
+++ src/models/mlp.py
@@ -1,25 +1,18 @@
 from tensorflow.keras.layers import Layer, Dropout, Dense
-from tensorflow.keras.activations import get as get_activation
 
 
 class MLPBlock(Layer):
-    def __init__(self, layer_units, dropout=None, activation=None, **kwargs):
+    def __init__(self, layer_units, dropout=None, activation=None, last_activation=None, **kwargs):
         super().__init__(**kwargs)
         self.hidden_layers = list()
-        self.activation = get_activation(activation)
-        if dropout:
-            for i in range(len(layer_units)):
+        self.number_of_layers = len(layer_units)
+        for i in range(self.number_of_layers):
+            if dropout:
                 self.hidden_layers.append(Dropout(dropout))
-                if layer_units[i] == 1:
-                    self.hidden_layers.append(Dense(1))
-                else:
-                    self.hidden_layers.append(Dense(layer_units[i], activation=activation))
-        else:
-            for i in range(len(layer_units)):
-                if layer_units[i] == 1:
-                    self.hidden_layers.append(Dense(1))
-                else:
-                    self.hidden_layers.append(Dense(layer_units[i], activation=activation))
+            self.hidden_layers.append(Dense(
+                layer_units[i],
+                activation=last_activation if i == self.number_of_layers - 1 else activation
+            ))
 
     def call(self, inputs, **kwargs):
         z = inputs
